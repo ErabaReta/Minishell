7d0dca6e126c83560cf23676be64edba2f9e7d4b
El Mokhtar Ouhrich <eouhrich@c4r8p9.1337.ma>
2024-05-30 19:50:56 +0100

    simple cleaning and being ready to start.

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eouhrich <eouhrich@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/30 15:01:09 by eouhrich          #+#    #+#             */
+/*   Updated: 2024/05/30 19:48:47 by eouhrich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
+
 
 int	main(void)
 {
-	
+	char *str;
+	printf("user >$ ");
+	fflush(stdout);
+	str = readline("");
+	while (str != NULL)
+	{
+		printf("your command is %s\n", str);
+		// free(str);
+		printf("user >$ ");
+		fflush(stdout);
+
+		str = readline("");
+	}
+	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -1,8 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eouhrich <eouhrich@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/30 15:01:05 by eouhrich          #+#    #+#             */
+/*   Updated: 2024/05/30 19:49:00 by eouhrich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <readline/readline.h>
+// #include <readline/history.h>
+
+# include "get_next_line/get_next_line_bonus.h"
 
 
 #endif
