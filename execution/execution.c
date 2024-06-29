03a1f6e5f2dbffeccc0702433c4c598beb5898d6
ErabaReta <mokhtarouhrich@gmail.com>
2024-06-29 10:03:51 +0100

    you can now just enter the cmd instead of entering the whole its absultoe path

--- execution/execution.c
+++ execution/execution.c
@@ -6,15 +6,111 @@
 /*   By: eouhrich <eouhrich@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 14:56:56 by eouhrich          #+#    #+#             */
-/*   Updated: 2024/06/04 16:23:54 by eouhrich         ###   ########.fr       */
+/*   Updated: 2024/06/29 10:01:18 by eouhrich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void execute_cmd()
+int includes_slash(char *cmd)
 {
+	int	i;
+
+	i = 0;
+	while (cmd[i] != '\0')
+	{
+		if (cmd[i] == '/')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*check_relative_path( char *cmd)
+{	
+	if (access(cmd, F_OK) == 0)
+	{
+		if (access(cmd, X_OK) == 0)
+		{
+			return (cmd);
+		}
+		else
+		{
+			printf("Minishell: permission denied: %s\n", cmd);
+			return (NULL);
+		}
+	}
+	else
+	{
+		printf("Minishell: command not found: %s\n", cmd);
+		return (NULL);
+	}
+}
+
+char	*check_paths(char **env, char *cmd)
+{
+	int	i;
+	char **paths;
+	char *tmp_path;
+	i = 0;
+	while (env[i] != NULL)
+	{
+		if (ft_strncmp(env[i], "PATH=", 5) == 0)
+			break ;
+		i++;
+	}
+	if (env[i] == NULL)
+	{
+		printf("Minishell: command not found: %s\n", cmd);
+		return (NULL);
+	}
+	paths = ft_split(&(env[i][5]), ':');
+	i = 0;
+	while (paths[i] != NULL)
+	{
+		tmp_path = ft_strjoin(ft_strjoin(paths[i], "/"), cmd);//TODO remove leak here
+		// printf("%s\n", tmp_path);
+		if (access(tmp_path, F_OK) == 0)
+		{
+			if (access(tmp_path, X_OK) == 0)
+				return tmp_path;
+			else
+			{
+				printf("minishell: permission denied: %s\n", cmd);
+				//TODO freee <----------------------
+				free(tmp_path);
+				return (NULL);
+			}
+		}
+		i++;
+		free(tmp_path);
+	}
+	printf("Minishell: command not found: %s\n", cmd);
+	return (NULL);
+}
+
+void execute_cmd(t_data *data,  char **env)
+{
+	char	*full_cmd;
+	char	**args;
 	
+	args = ft_split(data->cmd, ' '); // split the cmd to have it with its args
+	if (includes_slash(args[0]))// if the cmd include '/'  in it then execute it
+	{
+		full_cmd = check_relative_path(args[0]);
+	}
+	else 
+	{
+		full_cmd = check_paths(env, args[0]);
+	}
+	if (full_cmd == NULL)
+	{
+		//TODO free if there is somtihn to free (probably not)
+		exit(1);
+	}
+	// printf("full cmd is -> %s\n", full_cmd);
+	
+	execve(full_cmd, args, env);
 }
 
 void execution(t_data *data, int length, char **env)
@@ -24,10 +120,10 @@ void execution(t_data *data, int length, char **env)
 	int	**pipes;
 	t_data	*tmp;
 
-	if (length >= 2)
+	if (length >= 2) // if there is no '|' in the cmd there is no need to create pipe
 		pipes = (int **)malloc(sizeof(int *) * (length - 1));
 	i = 0;
-	while (i < length - 1)
+	while (i < length - 1) // allocating needed pipes and open them all (we will close the ones we dont need later)
 	{
 		pipes[i] = (int *)malloc(sizeof(int) * 2);
 		if (pipe(pipes[i]) == -1)
@@ -40,90 +136,69 @@ void execution(t_data *data, int length, char **env)
 	i = 0;
 	
 	tmp = data;
-
+	/*
+		creating child prossesse for each cmd then executing the cmd within it,
+		also if there pipes '|' changing the output for the cmd to be the input for the next cmd.
+	*/
 	while (i < length && tmp != NULL)
 	{
-		id = fork();
-		// pipes = (int *)malloc(sizeof(int) * 2);
-		// if (pipe(pipes) == -1)
-		// {
-		// 		printf("error : cant create pipe %d\n", i + 1);
-		// }
-		if (id == -1)
+		id = fork();// creating process
+		if (id == -1) // check if prossess created
 		{
 				printf("error : cant create process %d\n", i + 1);
 		}
-		if (id == 0)
+		if (id == 0) // if we are in the child proccess do this :
 		{
-			char	**args = ft_split(tmp->cmd, ' ');
 			if (length >= 2)
 			{
 				int j = 0;
-				while (j < length - 1)
+				while (j < length - 1) // closing all the not needed pipes
 				{
-					if (j != i )
+					if (j != i ) 
 					{
 						close(pipes[j][PIPE_INPUT]);
+						// printf("closing the input of pipe %d in child %d\n", j, i);
 					}
 					if ( j != i - 1)
 					{
 						close(pipes[j][PIPE_OUTPUT]);
+						// printf("closing the output of pipe %d in child %d\n", j, i);
 					}
 					j++;
 				}
-				//=
-				// if (i == 0)
-				// 	{
-				// 	close(pipes[i][PIPE_OUTPUT]);
-
-				// 	}
-				// else
-				// {
-					if (i != 0)
+					if (i != 0) // change the input to be the output of cmd before exept if this is the first cmd
 					{
-
 						dup2(pipes[i - 1][PIPE_OUTPUT], STDIN_FILENO);
-					close(pipes[i - 1][PIPE_OUTPUT]);
+						close(pipes[i - 1][PIPE_OUTPUT]);
+						// printf("changing the standard input of child %d to be ouput of pipe %d then close it\n", i, i - 1);
 					}
-				// }
-				// if (i == length - 1)
-				// 	close(pipes[i - 1][PIPE_INPUT]);
-				// else
-				// {
-					if (i != length - 1)
+					if (i != length - 1) // chande the output to be the input of the next cmd exept if this is the last cmd
 					{
-
+						// printf("changing the standard output of child  %d to be input of pipe %d then close it\n", i, i);
 						dup2(pipes[i][PIPE_INPUT], STDOUT_FILENO);
-					close(pipes[i][PIPE_INPUT]);
+						close(pipes[i][PIPE_INPUT]);
 					}
-				// }
 			}
-			execve(args[0], args, env);
+			// char	**args = ft_split(tmp->cmd, ' '); // split the cmd to have it with its args
+			execute_cmd(tmp, env);
+			// execve(args[0], args, env);
 		}
-		// else
-		// {
-		// 	if (length >= 2 )
-		// 	{
-		// 		close(pipes[PIPE_INPUT]);
-		// 	}
-		// }
 		tmp = tmp->next;
 		i++;
 	}
 	//=
 	int j = 0;
-	while (j < length - 1)
+	while (j < length - 1)// for the parent prosses I close all the pipe I created before
 	{
 			close(pipes[j][PIPE_OUTPUT]);
 			close(pipes[j][PIPE_INPUT]);
 		j++;
 	}
 	i = 0;
-	
-	while (i < length)
+	while (i < length) // wait for all the CMDs to be done the continue to give the prompt later
 	{
 		wait(NULL);
 		i++;
 	}
-	exit (0);
+	exit(0);
 }
---
+++ utils/ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eouhrich <eouhrich@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 16:29:09 by eouhrich          #+#    #+#             */
+/*   Updated: 2024/06/27 16:29:30 by eouhrich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ptr;
+	size_t	i;
+	size_t	j;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		return (ft_strdup(s2));
+	if (s2 == NULL)
+		return (ft_strdup(s1));
+	ptr = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	j = -1;
+	while (s2[++j] != '\0')
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+	return (ptr);
+}
