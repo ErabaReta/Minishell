dcbd1a6a50908d5ae19510bb85d77e2d6d28b942
ErabaReta <mokhtarouhrich@gmail.com>
2024-07-01 15:24:54 +0100

    the redirections < and > start working what ever its place in th cmd .. (multi infiles or outfiles not handled yet)

--- makefile
+++ makefile
@@ -1,9 +1,10 @@
 CC = cc
-CFLAGS = -Werror -Wextra -Wall #-g3 -fsanitize=address
+CFLAGS = -Werror -Wextra -Wall -g3 #-fsanitize=address
 RM = rm -rf
-EXEC_FILES =  ./execution/execution.c ./utils/ft_split.c ./utils/ft_memmove.c ./utils/ft_strncmp.c ./utils/ft_strjoin.c ./utils/ft_strlen.c ./utils/ft_strdup.c #get_next_line/get_next_line_bonus.c get_next_line/get_next_line_utils_bonus.c
-PARSING_FILES = ./parsing/mini_parsing.c
-CFILES = main.c ${EXEC_FILES} ${PARSING_FILES}
+UTILS_FILES = ./utils/ft_split.c ./utils/ft_memmove.c ./utils/ft_strncmp.c ./utils/ft_strjoin.c ./utils/ft_strlen.c ./utils/ft_strdup.c ./utils/char_in_cmd.c
+EXEC_FILES =  ./execution/execution.c execution/check_paths.c
+PARSING_FILES = ./parsing/mini_parsing.c ./parsing/redirectors.c
+CFILES = main.c ${EXEC_FILES} ${PARSING_FILES} ${UTILS_FILES}
 OFILES = ${CFILES:.c=.o}
 NAME = minishell
 
---
+++ utils/char_in_cmd.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   char_in_cmd.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eouhrich <eouhrich@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/30 16:49:27 by eouhrich          #+#    #+#             */
+/*   Updated: 2024/06/30 16:57:02 by eouhrich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+// returns the indice of a char inside a cmd (it ignore ones inside qoutes and double qoutes)
+int char_in_cmd(char *cmd, char	c)
+{
+	int	i;
+
+	i = 0;
+	while (cmd[i] != '\0')
+	{
+		if(cmd[i] == '\"')
+		{
+			while (cmd[i] != '\0' && cmd[i] != '\"')
+			{
+				i++;
+			}
+		}
+		if(cmd[i] == '\'')
+		{
+			while (cmd[i] != '\0' && cmd[i] != '\'')
+			{
+				i++;
+			}
+		}
+		if (cmd[i] == c)
+			return (i);
+		if (cmd[i] != '\0')
+			i++;
+	}
+	return (-1);
+}
