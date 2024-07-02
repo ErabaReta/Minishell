62885ffdf2872cbd4e71d1433101258ae962e57d
El Mokhtar Ouhrich <eouhrich@c3r2p4.1337.ma>
2024-07-02 14:36:34 +0100

    remove some redirections bugs

--- execution/exiter.c
+++ execution/exiter.c
@@ -6,13 +6,14 @@
 /*   By: eouhrich <eouhrich@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 10:07:52 by eouhrich          #+#    #+#             */
-/*   Updated: 2024/07/02 10:08:34 by eouhrich         ###   ########.fr       */
+/*   Updated: 2024/07/02 14:14:01 by eouhrich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	exiter()
+void	exiter(t_data *data, int code)
 {
-	exit(1);
+	(void)data;
+	exit(code);
 }
