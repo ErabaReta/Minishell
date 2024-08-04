1d3b45b66a51c9961163c4b324c1a49a8fc6fe5a
Ayoub Echchariy <ayechcha@c3r10p5.1337.ma>
2024-08-04 03:19:03 +0100

    UTILS NORM DONE

--- utils/ft_tabledup.c
+++ utils/ft_tabledup.c
@@ -1,21 +1,34 @@
-#include "../minishell.h"
-char  **ft_tabledup(char **table)
-{
-  int   i;
-  char  **res;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tabledup.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayechcha <ayechcha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/04 03:10:45 by ayechcha          #+#    #+#             */
+/*   Updated: 2024/08/04 03:10:47 by ayechcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-  i = 0;
-  if (table == NULL)
-    return (NULL);
-  while (table[i])
-    i++;
-  res = (char **)malloc(sizeof(char *) * (i + 1));
-  i = 0;
-  while (table[i])
-  {
-    res[i] = ft_strdup(table[i]);
-    i++;
-  }
-  res[i] = NULL;
-  return (res);
+#include "../minishell.h"
+
+char	**ft_tabledup(char **table)
+{
+	int		i;
+	char	**res;
+
+	i = 0;
+	if (table == NULL)
+		return (NULL);
+	while (table[i])
+		i++;
+	res = (char **)malloc(sizeof(char *) * (i + 1));
+	i = 0;
+	while (table[i])
+	{
+		res[i] = ft_strdup(table[i]);
+		i++;
+	}
+	res[i] = NULL;
+	return (res);
 }
