fc8a7d1d6e9a673bad2217afd250e2891d1ac269
Ayoub Echchariy <ayechcha@c3r10p6.1337.ma>
2024-08-30 06:38:34 +0100

    amigous ambigous fincho

--- parsing/ft_expand.c
+++ parsing/ft_expand.c
@@ -14,18 +14,33 @@
 
 int	heredoc(t_data *data, int *i, char **res, int j)
 {
+	char	quote;
+
 	if (ft_strncmp(data->files->redirection, "<<", 2) != 0)
 	{
 		while (data->files->file[*i])
-			quote_checker(data->files->file , res, i, 0);
-		if (data->files->file[0] != '\"' && data->files->file[0] != '\'')
+			quote_checker(data->files->file , res, i, 1);
+		if (*res == NULL)
+			data->files->heredoc_fd = -1;
+		while (*res && res[0][j])
 		{
-			if (*res == NULL)
+			if (res[0][j] == '\"' || res[0][j] == '\'')
+			{
+				quote = res[0][j];
+				j++;
+				while (res[0][j] != quote)
+					j++;
+				j++;
+			}
+			if (ft_iswhitespace(res[0][j]))
 				data->files->heredoc_fd = -1;
-			while (*res != NULL && res[0][j])
-				if (ft_iswhitespace(res[0][j++]) == 1)
-					data->files->heredoc_fd = -1;
+			if (res[0][j])
+				j++;
 		}
+		*res = NULL;
+		*i = 0;
+		while (data->files->file[*i])
+			quote_checker(data->files->file , res, i, 0);
 		if (data->files->heredoc_fd != -1)
 			data->files->file = ft_strdup(*res);
 		*res = NULL;
@@ -72,12 +87,11 @@ void	expand(t_data *data)
 	char	**res;
 	char	**args_res;
 
-	j = 0;
-	res = NULL;
-	args_res = NULL;
 	while (data)
 	{
 		i = 0;
+		res = NULL;
+		args_res = NULL;
 		while (data->args && data->args[i])
 		{
 			res = catch_expnad(data->args[i]);
