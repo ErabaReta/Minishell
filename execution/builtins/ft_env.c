b7d7dd7edc038568702181111cbe48118e382bf7
El Mokhtar Ouhrich <eouhrich@c3r9p5.1337.ma>
2024-08-31 20:11:17 +0100

    the big boss SHLVL is beated finally (I geuss ..)

--- execution/builtins/ft_env.c
+++ execution/builtins/ft_env.c
@@ -6,20 +6,31 @@
 /*   By: eouhrich <eouhrich@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 16:38:47 by eouhrich          #+#    #+#             */
-/*   Updated: 2024/07/23 20:15:59 by eouhrich         ###   ########.fr       */
+/*   Updated: 2024/08/31 19:31:27 by eouhrich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-void	ft_env(char **env)
+void	ft_env(char **env, int is_parent)
 {
 	int	i;
-
+	int new_shlvl;
+	
+	if (!is_parent && env_search("SHLVL") == NULL)
+		printf("SHLVL=0\n");
 	i = 0;
 	while (env != NULL && env[i] != NULL)
 	{
-		printf("%s\n", env[i]);
+		if (!is_parent && ft_strncmp(env[i], "SHLVL=", 6) == 0)
+		{
+			new_shlvl = ft_atoi(&env[i][6]);
+			if (new_shlvl <= 0)
+				new_shlvl = 0;
+			printf("SHLVL=%d\n", new_shlvl -1);
+		}
+		else
+			printf("%s\n", env[i]);
 		i++;
 	}
 }
