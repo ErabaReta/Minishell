315ca485ea2ef6a99b937e66ccf82b4237251ea5
ErabaReta <mokhtarouhrich@gmail.com>
2024-09-02 23:53:44 +0100

    cd & pwd (builtnis) arc : saga of cutting snake's head and wrath of the twins PWD and OLDPWD

--- execution/builtins/ft_exit.c
+++ execution/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: eouhrich <eouhrich@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 13:51:53 by eouhrich          #+#    #+#             */
-/*   Updated: 2024/08/18 20:45:57 by eouhrich         ###   ########.fr       */
+/*   Updated: 2024/09/02 16:47:41 by eouhrich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,10 @@ void	ft_exit(t_data *data, int is_parent, int *fd, int *exit_status)
 		if (!is_parent)
 			exiter(1);
 		else
+		{
+			*exit_status = 1;
 			return ;
+		}
 	}
 	else
 		pars_exit_arg(data->args[1]);
