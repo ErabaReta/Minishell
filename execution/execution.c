a2e864ee45f5f6eaa3722d05c50b7bab201d64f5
ErabaReta <mokhtarouhrich@gmail.com>
2024-09-04 17:10:38 +0100

    starting implimenting the fd pool to remove fd leaks of the herdoc in parent and childs

--- execution/execution.c
+++ execution/execution.c
@@ -6,11 +6,21 @@
 /*   By: eouhrich <eouhrich@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 14:56:56 by eouhrich          #+#    #+#             */
-/*   Updated: 2024/09/04 01:18:31 by eouhrich         ###   ########.fr       */
+/*   Updated: 2024/09/04 02:43:26 by eouhrich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+void handle_shlvl()
+{
+	if (env_search("SHLVL") != NULL && ft_atoi(env_search("SHLVL")->value) - 1 >= 1000)
+	{
+		print_3_err("minishell: warning: shell level ("
+						, ft_itoa(ft_atoi(env_search("SHLVL")->value) - 1, 0),
+						 ") too high, resetting to 1\n", -1);
+	}
+}
 
 //executes the cmd with its argiments after checking of its existence and its permissions
 void execute_cmd(t_data *data)
@@ -29,10 +39,11 @@ void execute_cmd(t_data *data)
 		print_err(": command not found\n");
 		exiter(127);
 	}
-	if (char_in_cmd(data->args[0], '/') != -1)// if the cmd include '/'  in it then execute it
+	if (char_in_cmd(data->args[0], '/') != -1)
 		full_cmd = check_relative_path(data->args[0]);
 	else 
 		full_cmd = check_paths(data->args[0]);
+	handle_shlvl();
 	if (execve(full_cmd, data->args, env_list_to_table()) == -1)
 	{
 		perror("minishell: ");
@@ -70,10 +81,10 @@ void	sig_exit_exev(int sig)
 void	close_unwanted(int a_pipe[2], int length, int *i, int *fd_out)
 {
 	if (*i != length - 1) 
-		close(a_pipe[PIPE_INPUT]);//
+		close(a_pipe[PIPE_INPUT]);
 	if (*i != 0)
 		close (*fd_out);
-	*fd_out = a_pipe[PIPE_OUTPUT];//
+	*fd_out = a_pipe[PIPE_OUTPUT];
 	*i += 1;
 }
 
@@ -88,13 +99,13 @@ void	childs_factory(t_data *tmp, int length, int *child_pids)
 	{
 		if (length >= 2 && i != length - 1)
 			pipe(a_pipe);
-		child_pids[i] = fork();// creating process
-		if (child_pids[i] == -1) // check if prossess created
+		child_pids[i] = fork();
+		if (child_pids[i] == -1)
 		{
 			perror("minishell ");
 			exiter(1);
 		}
-		if (child_pids[i] == 0) // if we are in the child proccess do this :
+		if (child_pids[i] == 0)
 		{
 			if (piping(a_pipe, length, i, fd_out) && tmp->files != NULL)
 					handle_files(tmp->files, 0);
@@ -106,7 +117,6 @@ void	childs_factory(t_data *tmp, int length, int *child_pids)
 	}
 }
 
-// takes the cmd line and execute or turns an error if it counter one
 void execution(t_data *data, int length)
 {
 	int	i;
@@ -114,15 +124,16 @@ void execution(t_data *data, int length)
 	int status;
 	
 	svars = get_specials();
-	if (length == 1 && check_builtins(data, 1) == 0) // check if there is no pipes and the cmd is a builtin so it executes it on the parent process
+	if (length == 1 && check_builtins(data, 1) == 0)
 		return ;
 	int *child_pids = (int *)mallocate(sizeof(int) * (length));
 	childs_factory(data, length, child_pids);
 	i = 0;
 	status = 0;
-	while (i < length) // wait for all the CMDs to be done the continue to give the prompt later
+	while (i < length)
 	{
-		// wait(NULL);
+		setup_signal_handler(1, SIG_IGN, sighandler_exev);
+
 		svars->child_p = waitpid(child_pids[i], &status, 0);
 		i++;
 	}
