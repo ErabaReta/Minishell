42582006e722b1081b0322e59d4298b8485e946e
Ayoub Echchariy <ayechcha@c3r9p9.1337.ma>
2024-09-12 17:59:19 +0100

    I think i am DONE

--- parsing/syntax_error.c
+++ parsing/syntax_error.c
@@ -6,23 +6,47 @@
 /*   By: ayechcha <ayechcha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 17:35:11 by hunter            #+#    #+#             */
-/*   Updated: 2024/08/30 03:22:10 by ayechcha         ###   ########.fr       */
+/*   Updated: 2024/09/12 17:46:19 by ayechcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+t_data	*check_errors(t_data *data)
+{
+	t_spec	*svars;
+
+	svars = get_specials();
+	if (!syntax_error_pipe(data) || !syntax_error_red(data))
+	{
+		svars->exit_status = 2;
+		return (NULL);
+	}
+	redirection(data);
+	if (!syntax_error_her(data))
+		return (NULL);
+	expand(data);
+	if (!expand_file(data))
+		return (NULL);
+	return (data);
+}
+
 t_data	*reds_check(char **reds, t_data *curr, int i)
 {
-	int	j;
+	int		j;
+	t_spec	*svars;
 
 	j = 0;
+	svars = get_specials();
 	while (reds[j])
 	{
 		if (ft_strncmp(curr->args[i], reds[j], 2) == 0
 			&& curr->args[i + 1] == NULL)
+		{
+			svars->exit_status = 2;
 			return (printf("syntax error near unexpected token %s\n"
 					, curr->args[i]), NULL);
+		}
 		j++;
 	}
 	return (curr);
@@ -32,17 +56,22 @@ t_data	*syntax_error_red(t_data *data)
 {
 	int		i;
 	t_data	*curr;
+	t_spec	*svars;
 	char	**reds;
 
 	reds = ft_split("< << > >>", ' ');
 	curr = data;
+	svars = get_specials();
 	while (curr)
 	{
 		i = 0;
 		while (curr->args[i])
 		{
 			if (reds_check(reds, curr, i) == NULL)
+			{
+				svars->exit_status = 2;
 				return (NULL);
+			}
 			i++;
 		}
 		curr = curr->next;
@@ -53,29 +82,34 @@ t_data	*syntax_error_red(t_data *data)
 t_data	*syntax_error_pipe(t_data *data)
 {
 	t_data	*curr;
+	t_spec	*svars;
 
 	curr = data;
+	svars = get_specials();
 	while (curr)
 	{
 		if (curr->next != NULL)
+		{
 			if (curr->next->args[0] == NULL || curr->args[0] == NULL)
+			{
+				svars->exit_status = 2;
 				return (printf("syntax error near unexpected token `|'\n")
 					, NULL);
+			}
+		}
 		curr = curr->next;
 	}
 	return (data);
 }
 
-t_data	*syntax_error_her(t_data *data)
+int	syntax_error_her(t_data *data)
 {
-	t_data			*first;
 	t_files_list	*curr;
 	t_spec			*svars;
-	int		her_num;
+	int				her_num;
 
 	svars = get_specials();
 	her_num = 0;
-	first = data;
 	while (data)
 	{
 		curr = data->files;
@@ -94,5 +128,5 @@ t_data	*syntax_error_her(t_data *data)
 		printf("maximum here-document count exceeded\n");
 		exiter(2);
 	}
-	return (first);
+	return (1);
 }
